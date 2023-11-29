8e64ac4c73764151922ba7fe6c3274c70dae169d
elhindkhalifi <elhindkhalifi@gmail.com>
2023-11-29 02:55:19 -0500

    creation acceuil + css

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inda's Clothing Company</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<button class="auth-button"><a href="./authentification/signup.php">Signup</a></button>
+<button class="auth-button"><a href="./authentification/login.php">Login</a></button>
+
+</body>
+</html>
