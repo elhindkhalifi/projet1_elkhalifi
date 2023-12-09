c41c8e027dd92df97f0cc56b3ae041af040f071e
elhindkhalifi <elhindkhalifi@gmail.com>
2023-12-08 20:40:10 -0500

    login marche + erreurs marchent

--- authentification/login.php
+++ authentification/login.php
@@ -1,13 +1,56 @@
-<center><h2>Welcome to Inda's</h2>
-<h2>Connexion</h2>
-<a href="../index.php">Retour Acceuil</a></center>
- 
-<form method="post" action="../results/loginResult.php">
+<?php
+session_start(); // Start the session
+
+
+
+if ($_SESSION['login_attempts'] >= 3) {
+        // TODO:BLOCK IP 
+    echo "Too many login attempts. Please try again later.";
+    exit;
+}
+$user_name = '';
+if (isset($_SESSION['login_form']['user_name'])) {
+    $user_name = $_SESSION['login_form']['user_name'];
+}
+$pwd = '';
+if (isset($_SESSION['login_form']['pwd'])) {
+    $pwd = $_SESSION['login_form']['pwd'];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <center>
+        <h2>Welcome to Inda's</h2>
+        <h2>Connexion</h2>
+        <a href="../index.php">Retour Accueil</a>
+    </center>
+
+    <form method="post" action="../results/loginResult.php">
         <label for="user_name">Nom d'utilisateur :</label>
-        <input type="text" id="user_name" name="user_name" required>
+        <input type="text" id="user_name" name="user_name" value="<?php echo $user_name ?>" >
+        <br>
+        <!-- afficher les erreurs -->
+        <p style="color: red; font-size: 0.8rem;">
+            <?php echo isset($_SESSION['login_errors']['user_name'])? $_SESSION['login_errors']['user_name'] : '1' ?>
+        </p>
         <br>
         <label for="pwd">Mot de Passe :</label>
-        <input type="password" id="pwd" name="pwd" required>
+        <input type="password" id="pwd" name="pwd" value="<?php echo $pwd ?>">
         <br>
-        <input type="submit" value="log in">
-</form>
+        <!-- afficher les erreurs -->
+        <p style="color: red; font-size: 0.8rem;">
+            <?php echo isset($_SESSION['login_errors']['pwd'])? $_SESSION['login_errors']['pwd'] : '' ?>
+        </p>
+        <br>
+        <input type="submit" value="Log In">
+    </form>
+</body>
+</html>
--- results/loginResult.php
+++ results/loginResult.php
@@ -1,1 +1,92 @@
-<
+<a href="../">Accueil</a>
+<h2>Login result</h2>
+<?php
+
+require_once("../config/connexion.php");
+require_once("../functions/validation.php");
+require_once("../functions/userCrud.php");
+require_once ('../functions/functions.php');
+
+session_start(); // Start the session
+
+//Authentification
+
+if (isset($_POST)) {
+    unset($_SESSION['login_errors']);
+    var_dump($_POST);
+
+    $fieldValidation = true;
+    $pwdError="";
+    $nameError="";
+        //vérifier si username dans DB
+        if (!empty($_POST['user_name'])) {
+            $userData = getUserByUsername($_POST['user_name']);
+            //si l'utilisateur exist dans la DB
+            if ($userData) {
+                // comparer pwd avec DB (version encodée)
+                $saltedpwd=addSalt($_POST['pwd']);
+                $enteredPwdEncoded = encodePwd($saltedpwd);
+                if ($userData['pwd'] == $enteredPwdEncoded) {
+                    $fieldValidation = true;
+                    // Generate and save a token
+                    $token = hash('sha256', random_bytes(32));
+            
+                    // Save the token in the session
+                    $_SESSION['token'] = $token;
+            
+                    // Save the token in the database (update the user's token)
+                    updateUserToken($userData['id'], $token);
+            
+                    echo '</br></br>Mon token : </br>';
+                    var_dump($token);
+            
+                }else {
+                    $fieldValidation = false;
+                    $_SESSION['login_attempts']++;
+                    // Display an error message
+                    $pwdError= "Mot de passe incorrect. Essaie ".$_SESSION['login_attempts']." sur 3.<br> <a href='resetPwd.php'>mot de passe oublie?</a> ";
+                
+            }}else{
+                $fieldValidation = false;
+                $nameError="aucun utilisateur trouve avec ce nom dutilisateur";
+                } 
+        }elseif (empty($_POST['user_name'])) {
+            $fieldValidation = false;
+            $nameError="Ce champ est requis";
+
+        }elseif(empty($_POST['pwd'])){
+             $fieldValidation = false;
+            $pwdError="Ce champ est requis";
+             }
+         if ($fieldValidation==false){
+             $_SESSION['login_errors'] = [
+                 'user_name' =>  $nameError,
+                 'pwd'=> $pwdError,
+             ];
+             // Redirect vers login
+             $url = '../authentification/login.php';
+             header('Location: ' . $url);
+             var_dump($_SESSION['login_errors']);
+        }else{
+            $url = '../home.php';
+            header('Location: ' . $url);
+        }
+    }
+
+else {
+    //redirect vers login
+    $url = '../authentification/login.php';
+    header('Location: ' . $url);
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
