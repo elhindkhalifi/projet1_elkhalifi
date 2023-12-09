e938326e8066d2a0a62469c7ce5686773ac7914e
elhindkhalifi <elhindkhalifi@gmail.com>
2023-12-08 22:46:56 -0500

    home page +css

--- home.php
+++ home.php
@@ -1,5 +1,137 @@
-<?Php
+<?php
 include "./public/header.php";
-include "./public/footer.php";
-
 ?>
+<div style="display: flex;">
+
+<!-- Sidebar Section -->
+<aside style="background-color: #2D2D2D; color: #ffffff; padding: 20px; text-align: center; width: 300px;">
+     <h2>Explore More</h2>
+     <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+               <br>
+  	 			<h4>Products</h4>
+  	 			<ul>
+                    <li><a href="#hats">Hats</a></li>
+                    <li><a href="#scarves">Scarves</a></li>
+                    <li><a href="#gloves">Gloves</a></li>
+                    <li><a href="#jackets">Winter Jackets</a></li>
+                    <li><a href="#shoes">Shoes</a></li>
+  	 			</ul><br>
+                   <h4>Profile</h4>
+  	 			<ul>
+  	 				<li><a href="#">Profile Info</a></li>
+  	 				<li><a href="#">Reset your password</a></li>
+                    <li><a href="#">Logout</a></li>
+  	 			</ul>
+                <br>
+                   <h4>Follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		
+  	 		
+</aside>
+<section>
+<!-- Stylish Welcome Section -->
+<section style="background-color: #BDC4CD; padding: 50px; text-align: center;">
+    <div class="container">
+        <h2>Welcome to Inda's Winter Clothing</h2>
+        <p>
+            Founded by Hind Elkhalifi, a visionary college student at Teccart College in Montreal, Inda's Winter Clothing sets the stage for a new era in winter fashion. We are dedicated to crafting high-quality, stylish, and comfortable clothing that keeps you warm and confident throughout the season.
+        </p>
+        <p>
+            Explore our curated collections, featuring trendy jackets, cozy scarves, and more. Indulge in warmth with confidence and style.
+        </p>
+    </div>
+    </section>
+
+
+
+<!-- Our Commitment to Quality -->
+<section style="background-color: #BDC4CD; padding: 50px; text-align: center;">
+    <div class="container">
+        <h2>Our Commitment to Quality</h2>
+        <p>
+            At Inda's, quality is non-negotiable. We source the finest materials and employ skilled artisans to ensure each garment meets the highest standards of craftsmanship.
+        </p>
+        <p>
+            From stitching to finishing touches, every detail is a testament to our commitment to delivering excellence in durability and style.
+        </p>
+    </div>
+</section>
+
+<!-- Explore Our Collections -->
+<section style="background-color: #BDC4CD; padding: 50px; text-align: center;">
+    <div class="container">
+        <h2>Explore Our Collections</h2>
+        <p>
+            Discover a world of winter fashion at Inda's. Our curated collections feature a diverse range of styles, colors, and textures to suit every taste.
+        </p>
+        <p>
+            From statement jackets to cozy scarves, each piece is designed to elevate your cold-weather wardrobe.
+        </p>
+    </div>
+</section>
+
+<!-- Our Story Section -->
+<section style="background-color: #24262A; color: #ffffff; padding: 50px; text-align: center;">
+    <div class="container">
+        <h2>Our Story</h2>
+        <p>
+            Inda's Winter Clothing was born out of the creative vision of Hind Elkhalifi, a passionate college student at Teccart College in Montreal. Fueled by a desire to redefine winter fashion in Montreal, Hind embarked on a journey to create a brand that blends style, comfort, and warmth.
+        </p>
+        <p>
+            With a relentless pursuit of excellence, Inda's has evolved into a symbol of fashion-forward winter wear, catering to those who seek both functionality and elegance in their cold-weather wardrobe.
+        </p>
+    </div>
+</section>
+
+</section>
+</div>
+
+<!-- Our Team Section -->
+<section style="background-color: #BDC4CD; padding: 50px; text-align: center;">
+    <div class="container">
+        <h2>Our Team</h2>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+           
+                    <div class="container"  style="background-color: white">
+                        <h2>Hind Elkhalifi</h2>
+                        <p class="title">CEO & Founder</p>
+                        <p>
+                            Hind Elkhalifi, the visionary founder of Inda's Winter Clothing, is a college student at Teccart College in Montreal. With a creative spark and a pursuit of innovation, Hind blends fashion and warmth, setting new standards for winter wear in Montreal.
+                        </p>
+                        <p>Email: elhindkhalifi@gmail.com</p>
+                        <p><button class="button">Contact</button></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="card">
+                   
+                    <div class="container" style="background-color: white">
+                        <h2>Julian Dussollier</h2>
+                        <p class="title">PHP College Professor</p>
+                        <p>
+                            Julian Dussollier, a dedicated PHP expert and college professor, played a pivotal role in inspiring Hind Elkhalifi to embark on the journey of creating Inda's Winter Clothing. His expertise and guidance contributed to the realization of this innovative venture.
+                        </p>
+                        <p>Email: julian@julian.ca</p>
+                        <p><button class="button">Contact</button></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include "./public/footer.php";
+?>
+
--- public/header.php
+++ public/header.php
@@ -1,58 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inda's Winter Clothing</title>
-    <!-- Add your CSS styles or link to an external stylesheet here -->
-    <link rel="stylesheet" href="../public/style.css">
-    <style>
-        body {
-            line-height: 1.5;
-            font-family: 'Poppins', sans-serif;
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
-        header {
-            background-color: #4D6881;
-            padding: 10px;
-            color: white;
-            text-align: center;
-        }
-
-        nav {
-            background-color: #4D6870;
-            padding: 10px;
-            color: white;
-            text-align: center;
-        }
-
-        nav a {
-            margin-right: 20px;
-            color: white;
-            text-decoration: none;
-        }
-
-        nav a:hover {
-            text-decoration: underline;
-        }
-    </style>
+  <title>Header Design</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="./public/style.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
 <body>
 
-<header>
-    <div style="background-color: #4D6881; padding: 10px; color: white; text-align: center;">
-        <h1>Inda's Winter Clothing</h1>
-    </div>
-    <nav style="background-color: #4D6870; padding: 10px; color: white; text-align: center;">
-        <a href="../shop.php" style="margin-right: 20px; color: white; text-decoration: none;">Shop</a>
-        <a href="../cart.php" style="margin-right: 20px; color: white; text-decoration: none;">Cart</a>
-        <a href="../logout.php" style="color: white; text-decoration: none;">Logout</a>
-    </nav>
-</header>
-
+  <header class="header">
+     <div class="container">
+      <div class="row">
+        <div class="header-col">
+          <h1>Inda's clothing company</h1>
+        </div>
+        <div class="header-col">
+        <ul><h1>Welcome to Inda's online website </h1></ul>
+        </div>
+        <div class="header-col">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        </div>
+      </div>
+     </div>
+  </header>
 
 </body>
 </html>
+
